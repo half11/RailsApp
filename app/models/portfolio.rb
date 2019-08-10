7656dc78f4ddb94b5ef26c2b58eef99d7f8b6318
Fred CM <fredoacmk@gmail.com>
2019-08-10 00:37:16 -0500

    image defaults in model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
     end
 
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Angular') }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        # ||= significa if self.main_image == nil  self.main_image = "https://via.placeholder.com/600x400C/O%20https://placeholder.com/"
+        self.main_image ||= "https://via.placeholder.com/600x400C/O%20https://placeholder.com/"
+        self.thumb_image ||= "https://via.placeholder.com/350x200C/O%20https://placeholder.com/"
+    end
 end
