82236c8ecce3509f2947c75d2d53be7767af7556
Fred CM <fredoacmk@gmail.com>
2019-08-10 00:20:21 -0500

    custom scopes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,6 +2,7 @@ class PortfoliosController < ApplicationController
 
     def index
         @portfolio_items = Portfolio.all
+        #@portfolio_items = Portfolio.ruby_on_rails_portfolio_items # Portfolio.angular
     end
 
     def new
