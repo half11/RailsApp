52676f17546d4ee3223cc471eb7e5a91ec6a9e33
Alfredo <fredoacmk@gmail.com>
2019-09-20 23:46:26 -0500

    Implemented twitter call on tech new page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about' #controller / method - aqui se puede usar la ruta que sea y no afecta la funcionalidad
   get 'pages/contact' #controller / method - aqui no se puede cambiar la ruta pues pierde la secuencia que usa rails, si queremos cambiar la ruta se debe usar la forma de arriba
+  get 'tech-news', to: 'pages#tech_news' 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
