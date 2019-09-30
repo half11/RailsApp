78a1415220385d474434984510efd5fdee9671dc
Alfredo <fredoacmk@gmail.com>
2019-09-30 00:02:58 -0500

    type mistake bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
     def create
-        @comment = current_user.comment.build(comment_params)
+        @comment = current_user.comments.build(comment_params)
     end
 
     private
