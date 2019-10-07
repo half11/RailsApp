0a801663d8196710bf48af582d1455b4df6986f5
Alfredo <fredoacmk@gmail.com>
2019-10-07 00:35:57 -0500

    production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://acm-portfolio.herokuapp.com', 'http://acm-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://acm-portfolio.herokuapp.com/cable"
 end
